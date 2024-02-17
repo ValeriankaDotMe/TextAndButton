89d98cf2ae9742dfc271f5bd90821dff3bc9fd32
milaymrazi <veronikacat2011@mail.ru>
2024-02-17 22:15:56 +0500

    New UI And Functionality

--- MainForm.cs
+++ MainForm.cs
@@ -1,15 +1,15 @@
 namespace TextAndButton
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
 
         private void showText_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello, World!");
+            MessageBox.Show(messageField.Text);
         }
     }
 }
